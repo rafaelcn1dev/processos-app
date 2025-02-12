df7a6357e76a7840fddb5484369366ff27ddb022
rafaelcn1dev <rafael.cunha@senior.com.br>
2025-02-12 00:01:01 -0300

    nav component alinhado no css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module'; // Importe o módulo de roteamento
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
+  standalone: true,
+  imports: [MatSidenavModule, MatButtonModule, MatListModule, MatIconModule]
 })
+  
 export class NavComponent implements OnInit {
 
   constructor() { }
