d80d4f60d30fd06b778c43e78656fc40cb95fa05
rafaelcn1dev <rafael.cunha@senior.com.br>
2025-02-12 11:36:31 -0300

    criado rota para lista de processos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule  } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProcessosListComponent } from './components/processos/processos-list/processos-list.component';
 
 
 const routes: Routes = [
   {
     path: '', component: NavComponent, children: [
-      { path: 'home', component: HomeComponent }
+      { path: 'home', component: HomeComponent },
+      { path: 'processos', component: ProcessosListComponent }
     ]
   }
 ];
